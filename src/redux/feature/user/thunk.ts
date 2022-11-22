7e20ac011508277646d881b6e515c2e9dbca6088
ducth5 <ducth5@vng.com.vn>
2022-11-22 16:58:48 +0700

    add redux thunk base!

--- src/redux/feature/user/thunk.ts
+++ src/redux/feature/user/thunk.ts
@@ -1,1 +1,35 @@
-export const  api = 1
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import UserApi from '@/api/userApi';
+
+const getUserById = createAsyncThunk('user/fetchUserById', async (id: number) => {
+  const response = await UserApi.getUserById(id);
+  // The value we return becomes the `fulfilled` action payload
+  return response;
+});
+
+const getUserList = createAsyncThunk('user/getUserList', async () => {
+    const response = await UserApi.getAll();
+    // The value we return becomes the `fulfilled` action payload
+    return response;
+});
+
+
+export const userExtraReducers = (builder: any)=>{
+    builder
+    .addCase(getUserById.pending, (state: any) => {
+      state.status = 'loading';
+    })
+    .addCase(getUserById.fulfilled, (state: any, action: any) => {
+      state.status = 'idle';
+    })
+    .addCase(getUserById.rejected, (state: any) => {
+      state.status = 'failed';
+    });
+}
+
+const UserThunks = {
+    getUserById,
+    getUserList
+}
+
+export default UserThunks;
