c2369b0ccc1981cb5278e5ce8da1ff3f202bcc41
tramhuuducvn <tramhuuduc.vn@gmail.com>
2022-11-23 17:22:07 +0700

    add ui flow login, register, formik lib!

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,27 @@
 import {useAppDispatch} from '@/redux';
 import {authActions} from '@/redux/feature/auth/slice';
-import {useState} from 'react';
-import {Navigate} from 'react-router-dom';
+import {useMemo, useState} from 'react';
+import {Link, Navigate} from 'react-router-dom';
+import {useFormik} from 'formik';
 
 export default function Login() {
   const [token, setToken] = useState<string | null>(null);
   const dispatcher = useAppDispatch();
+
+  const initialValues = useMemo(() => {
+    return {
+      email: '',
+      password: '',
+    };
+  }, []);
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: values => {
+      console.log('@DUKE__onSubmit', values);
+      formik.setValues(initialValues);
+    },
+  });
 
   const submitLogin = async () => {
     setTimeout(() => {
@@ -16,8 +32,29 @@ export default function Login() {
 
   return (
     <div>
-      <button onClick={submitLogin}>Logidn</button>
-      {token && <Navigate to={'/'} state={'abc'} replace />}
+      <form onSubmit={formik.handleSubmit}>
+        {/* Email */}
+        <label htmlFor="email">EmailAddress: </label>
+        <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+        <br />
+        <br />
+        {/* Password */}
+        <label htmlFor="email">Password: </label>
+        <input id="password" name="password" type="password" onChange={formik.handleChange} value={formik.values.password} />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+
+      <div>
+        <br />
+        <a href="">login with google!</a>
+        <br />
+        <br />
+        <a href="">login with google!</a>
+      </div>
+      <Link to={'/register'}>Register</Link>
+      {token && <Navigate to={'/'} />}
     </div>
   );
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,58 @@
-import React from 'react';
+import {useAppDispatch} from '@/redux';
+import {authActions} from '@/redux/feature/auth/slice';
+import {useMemo, useState} from 'react';
+import {Navigate} from 'react-router-dom';
+import {useFormik} from 'formik';
 
 export default function Register() {
-  return <div>Register</div>;
+  const [token, setToken] = useState<string | null>(null);
+  const dispatcher = useAppDispatch();
+
+  const initialValues = useMemo(() => {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }, []);
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: values => {
+      console.log('@DUKE__onSubmit', values);
+      formik.setValues(initialValues);
+    },
+  });
+
+  const submitLogin = async () => {
+    setTimeout(() => {
+      setToken('user-1');
+      dispatcher(authActions.loginSucceed());
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        {/* Email */}
+        <label htmlFor="email">EmailAddress: </label>
+        <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+        <br />
+        <br />
+        {/* Password */}
+        <label htmlFor="email">Password: </label>
+        <input id="password" name="password" type="password" onChange={formik.handleChange} value={formik.values.password} />
+        <br />
+        <br />
+        {/* Confirm Password */}
+        <label htmlFor="email">Confirm Password: </label>
+        <input id="password" name="password" type="password" onChange={formik.handleChange} value={formik.values.confirmPassword} />
+        <br />
+        <br />
+        <button type="submit">Register</button>
+      </form>
+
+      {token && <Navigate to={'/'} />}
+    </div>
+  );
 }
