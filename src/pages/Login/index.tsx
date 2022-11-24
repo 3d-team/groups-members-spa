bb9ca251cfc5703a4faa4e5889a075a3239a2828
ducth5 <ducth5@vng.com.vn>
2022-11-24 17:35:27 +0700

    saved changes!

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,66 +1,3 @@
-// import {useAppDispatch} from '@/redux';
-// import {authActions} from '@/redux/feature/auth/slice';
-// import {useMemo, useState} from 'react';
-// import {Link, Navigate} from 'react-router-dom';
-// import {useFormik} from 'formik';
-// import Button from '@material-ui/core/Button';
-// import styles from './styles.module.css'
-
-// export default function Login() {
-//   const [token, setToken] = useState<string | null>(null);
-//   const dispatcher = useAppDispatch();
-
-//   const initialValues = useMemo(() => {
-//     return {
-//       email: '',
-//       password: '',
-//     };
-//   }, []);
-
-//   const formik = useFormik({
-//     initialValues,
-//     onSubmit: values => {
-//       console.log('@DUKE__onSubmit', values);
-//       formik.setValues(initialValues);
-//       submitLogin();
-//     },
-//   });
-
-//   const submitLogin = async () => {
-//     setTimeout(() => {
-//       setToken('user-1');
-//       dispatcher(authActions.loginSucceed());
-//     }, 3000);
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <form onSubmit={formik.handleSubmit}>
-//         {/* Email */}
-//         <label htmlFor="email">EmailAddress: </label>
-//         <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
-//         <br />
-//         <br />
-//         {/* Password */}
-//         <label htmlFor="email">Password: </label>
-//         <input id="password" name="password" type="password" onChange={formik.handleChange} value={formik.values.password} />
-//         <br />
-//         <br />
-//         <Button variant='contained' color='primary' type={'submit'}>Login</Button>
-//       </form>
-//       <div>
-//         <br />
-//         <a href="">login with google!</a>
-//         <br />
-//         <br />
-//         <a href="">login with google!</a>
-//       </div>
-//       <Link to={'/register'}>Register</Link>
-//       {token && <Navigate to={'/'} />}
-//     </div>
-//   );
-// }
-
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -74,15 +11,14 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Copyright from '@/components/Copyright';
-import useStyles from './styles';
 import { useFormik } from 'formik';
 import { useMemo, useState } from 'react';
 import { useAppDispatch } from '@/redux';
 import { authActions } from '@/redux/feature/auth/slice';
+import useStyles from './styles';
 
 const Login = () => {
   const classes = useStyles();
-  
 
   const [token, setToken] = useState<string | null>(null);
   const dispatcher = useAppDispatch();
@@ -114,7 +50,7 @@ const Login = () => {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.paper}>
+        <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
@@ -132,6 +68,7 @@ const Login = () => {
             autoComplete="email"
             autoFocus
             value={formik.values.email}
+            onChange={formik.handleChange}
           />
           <TextField
             variant="outlined"
@@ -144,6 +81,7 @@ const Login = () => {
             id="password"
             autoComplete="current-password"
             value={formik.values.password}
+            onChange={formik.handleChange}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
@@ -180,3 +118,5 @@ const Login = () => {
 }
 
 export default Login;
+
+
