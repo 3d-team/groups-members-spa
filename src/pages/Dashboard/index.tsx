efb83dc28ebdbea26cf5a1180c4fb1a973351d8e
lvd318 <levandinh318@gmail.com>
2022-11-27 14:53:19 +0700

    ui-dashboard: list classes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 import Drawer from '@/components/Drawer/Drawer';
+import ClassCard from '@/components/ClassCard/ClassCard';
+import { ClassModel } from '@/models/class';
+import Grid from "@mui/material/Grid";
+
 
 export default function Dashboard() {
+  const [classes, setClasses] = useState<ClassModel[]>([]);
+
+  //Get class
+
   return (
     <>
-        <Drawer />
+      <Drawer />
+      {classes.length === 0 ? (
+        <div>
+          No classes found! Join or create one!
+        </div>
+      ) : (
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          px={5}
+        >
+          {classes.map((individualClass) => (
+            <ClassCard
+              creatorName={individualClass.creatorName}
+              name={individualClass.name}
+              subjectName={individualClass.subjectName}
+              id={individualClass.id}
+            />
+          ))}
+        </Grid>
+      )}
     </>
-    );
+  );
 }
